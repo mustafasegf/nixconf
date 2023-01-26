5dc020cea7bca73f618436fccd7fa5e43d6cff0c
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-01-26 20:57:32 +0700

    chore: removed deadcode

--- home.nix
+++ home.nix
@@ -13,44 +13,6 @@
 
   xdg.enable = true;
 
-  # pkgs.mkShell = rec {
-  #   buildInputs = with pkgs; [
-  #     llvmPackages_latest.llvm
-  #     llvmPackages_latest.bintools
-  #     zlib.out
-  #     rustup
-  #     xorriso
-  #     qemu
-  #     grub2
-  #     llvmPackages_latest.lld
-  #     python3
-  #   ];
-  #   RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
-  #   LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-  #   HISTFILE = toString ./.history;
-  #   shellHook = ''
-  #     export PATH=$PATH:${CARGO_HOME:-~/.cargo}/bin
-  #     export PATH=$PATH:${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-  #   '';
-  #
-  #   RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-  #     pkgs.libvmi
-  #   ]);
-  #
-  #   BINDGEN_EXTRA_CLANG_ARGS =
-  #     # Includes with normal include path
-  #     (builtins.map (a: ''-I"${a}/include"'') [
-  #       pkgs.libvmi
-  #       pkgs.glibc.dev
-  #     ])
-  #     # Includes with special directory paths
-  #     ++ [
-  #       ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-  #       ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-  #       ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-  #     ];
-  # };
-
   programs.zsh = {
     enable = true;
     autocd = true;
